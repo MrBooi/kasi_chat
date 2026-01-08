c68cb71db3c6ce457308ce2ca897f52a18ac6cda
MrBooi <ayabongabooi2@gmail.com>
2026-01-09 00:49:04 +0200

    feat: add app shared widgets and extensions

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,5 +1,6 @@
-export './constants/constants.dart';
 export 'colors/app_colors.dart';
+export 'constants/constants.dart';
+export 'extensions/extensions.dart';
 export 'spacing/app_spacing.dart';
 export 'theme/theme.dart';
 export 'typography/typography.dart';
